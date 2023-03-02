03eea6133cb22a062b8669660b54786bc7f5b54b
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-03-03 07:00:24 +1100

    SearchView and DataStore

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoRepository.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoRepository.kt
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.photogallery
 
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import com.bignerdranch.android.photogallery.api.GalleryItem
+import com.bignerdranch.android.photogallery.api.PhotoInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
@@ -9,12 +12,20 @@ class PhotoRepository {
     private val flickrApi: FlickrApi
 
     init {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
         flickrApi = retrofit.create<FlickrApi>()
     }
 
     suspend fun fetchPhotos() = flickrApi.fetchPhotos().photos.galleryItems
+
+    suspend fun searchPhotos(query: String): List<GalleryItem> =
+        flickrApi.searchPhotos(query).photos.galleryItems
 }
